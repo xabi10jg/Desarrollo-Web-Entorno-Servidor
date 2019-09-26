7a18f3e5b230eaad5c7217e957c5d3afb314b884
xabi10jg <xjacobgo18dw@ikzubirimanteo.com>
2019-09-26 09:35:45 +0200

    Bloque 1 ejercicio 4

--- EjerciciosBloque1/Ejercicio4.php
+++ EjerciciosBloque1/Ejercicio4.php
@@ -5,8 +5,26 @@
 </head>
 <body>
 	<?php
-	
-	?>
+
+$frase = "Mi nombre es Xabi";
+
+$separar = explode(" ", strtolower($frase));
+
+foreach($separar as $palabra)
+{
+    trim($palabra);
+    $nuevo .= $palabra; 
+}
+
+if($nuevo == strrev($nuevo))
+{
+    echo "La frase/palabra '".$frase."' es palíndromo";
+}
+else {
+    echo "La frase/palabra '".$frase."' no es palíndromo";
+}
+
+?>
 
 </body>
 </html>
